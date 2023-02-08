090e3bd117e0b81a27bc4fbfbac84136fab29995
Prashant Choudhary <prash.sastra1@gmail.com>
2023-02-08 22:49:12 +0530

    BookCheckoutPage Updates

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -38,23 +38,59 @@ export const BookCheckoutPage = () => {
         })
     }, []);
 
-    if(isLoading){
+    if (isLoading) {
         return (
             <SpinnerLoading />
         );
     }
 
-    if(httpError){
+    if (httpError) {
         return (
             <div className='container m-5'>
-                <p>{ httpError }</p>
+                <p>{httpError}</p>
             </div>
         );
     }
 
     return (
         <div>
-            <h3>Hi Java</h3>
+            <div className='container d-none d-lg-block'>
+                <div className='row mt-5'>
+                    <div className='col-sm-2 col-md-2'>
+                        {book?.img ?
+                            <img src={book?.img} width='226' height='349' alt='Book' />
+                            :
+                            <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226' height='349' alt='Book' />
+                        }
+                    </div>
+                    <div className='col-4 col-md-4 container'>
+                        <div className='ml-2'>
+                            <h2>{book?.title}</h2>
+                            <h5 className='text-primary'>{book?.author}</h5>
+                            <p className='lead'>{book?.description}</p>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+            </div>
+            <div className='container d-lg-none mt-5'>
+                <div className='d-flex justify-content-center align-items-center'>
+                    {
+                        book?.img ?
+                            <img src={book?.img} width='226' height='349' alt='Book' />
+                            :
+                            <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226' height='349' alt='Book' />
+                    }
+                </div>
+                <div className='mt-4'>
+                    <div className='ml-2'>
+                        <h2>{book?.title}</h2>
+                        <h5 className='text-primary'>{book?.author}</h5>
+                        <p className='lead'>{book?.description}</p>
+                    </div>
+                </div>
+                <hr />
+            </div>
         </div>
     );
 }
