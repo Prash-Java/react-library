f95ab1323de6701f72e8a3e97c9773b5c0c3ab70
Prashant Choudhary <prash.sastra1@gmail.com>
2023-02-09 21:37:21 +0530

    RatingReview Logic For Checkout

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { StarsReview } from "../Utils/StarsReview";
 
 export const BookCheckoutPage = () => {
 
@@ -68,6 +69,7 @@ export const BookCheckoutPage = () => {
                             <h2>{book?.title}</h2>
                             <h5 className='text-primary'>{book?.author}</h5>
                             <p className='lead'>{book?.description}</p>
+                            <StarsReview rating={5} size={32} />
                         </div>
                     </div>
                 </div>
@@ -87,6 +89,7 @@ export const BookCheckoutPage = () => {
                         <h2>{book?.title}</h2>
                         <h5 className='text-primary'>{book?.author}</h5>
                         <p className='lead'>{book?.description}</p>
+                        <StarsReview rating={5} size={32} />
                     </div>
                 </div>
                 <hr />
